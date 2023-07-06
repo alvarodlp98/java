ec426d66907ef97ae07dede46a30f73eb862371c
alvarodlp98 <110502302+alvarodlp98@users.noreply.github.com>
2023-07-06 17:09:38 -0300

    java

---
+++ src/main/java/com/proyecto/Booking/controller/BookController.java
@@ -0,0 +1,31 @@
+package com.proyecto.Booking.controller;
+
+import com.proyecto.Booking.persistence.entities.Book;
+import com.proyecto.Booking.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/books")
+@CrossOrigin(origins = "http://localhost:3000")
+public class BookController {
+
+    @Autowired
+    BookService bookService;
+
+    @PostMapping
+    public ResponseEntity<Book> create(@RequestBody Book book){
+        return new ResponseEntity<>(bookService.save(book), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Book>> findAll(){
+
+        return ResponseEntity.ok(bookService.findAll());
+    }
+}
